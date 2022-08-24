c77a1df916a8907cf2df3814a221cf339e67ca6f
KHANTSITHUZAWPC\KhantSithuZaw <scm.khantsithuzaw@gmail.com>
2022-08-24 10:33:05 +0630

    simple ifelse

---
+++ condition/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title = 'Conditional statements';
+  show = 'true';
+  display = 'no';
+}
